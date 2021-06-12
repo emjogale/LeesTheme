db9d8fd2563d4dc00a3e1832ac3eba5c4a027982
Emma Gale <emmajgale@hotmail.com>
2021-06-12 08:18:44 +0100

    add in new about section and change id from 8 to 1673 and category array from 2,3,4 to 2,7,5

--- LeesTheme/includes/section-frontpage.php
+++ LeesTheme/includes/section-frontpage.php
@@ -3,7 +3,7 @@
 	<div class="card d-flex border-0 text-dark">
 		<div class="row no-gutters">
 			<div class="col-12 col-md-4 d-flex align-items-center">
-			   <a href="index.php?page_id=8"> 
+			   <a href="index.php?page_id=1673"> 
 				<?php if( has_post_thumbnail() ):?>
 					<img src="<?php the_post_thumbnail_url('blog-small');?>" 
 						alt="<?php the_title(); ?>" 
@@ -14,10 +14,9 @@
 			<div class="col-12 col-md-8">	
 				<div class="card-body">
 					<div class="blog-content">
-					    
-						<?php the_excerpt();?>
-
-						<a class="text-secondary font-italic font-weight-bold" href="index.php?page_id=8">(Read more)</a>
+					    <p>
+							If we’ve shared a workspace, you'll know me for my long-standing appreciation of New Order, World War II knowledge and biscuit generosity. I’m a features writer, advertorial writer and sub-editor with a career that includes full-time positions at <i>GQ</i>, <i>Motor Sport</i>, <i>Grand Designs</i>, <i>Jack</i>, <i>Front</i> and <i>Amateur Photographer</i>, and freelance stints at <i>The Week Junior</i>, <i>Monocle</i>, <i>Men’s Health</i>, <i>NME</i>, <i>The Guide</i> (Guardian) and <i>Wired</i>. I’ve wangled interviews with many of my heroes and written articles on all sorts of niche subjects such as football shirts, electricity pylons and concrete lamp-posts – some of which you’ll find here. lg@leegale.co.uk.
+						</p>
 					</div>
 				</div>
 			</div>
@@ -33,7 +32,7 @@ endif; ?>
 $args = array(
    'post_type' => 'post',
    'posts_per_page' => 3,
-   'category__in'=> array(2,3,4)
+   'category__in'=> array(2,7,5)
    
    );
 
