4a3686b4b48c66d4dea31a1941b7a2c90b2299f1
Emma Gale <emmajgale@hotmail.com>
2025-02-16 18:48:40 +0000

    add gitignore

--- LeesTheme/includes/section-frontpage.php
+++ LeesTheme/includes/section-frontpage.php
@@ -1,68 +1,69 @@
-<?php if ( have_posts() ) : while( have_posts() ): the_post(); ?>
+<?php if (have_posts()) : while (have_posts()): the_post(); ?>
 
-	<div class="card d-flex border-0 text-dark">
-		<div class="row no-gutters">
-			<div class="col-12 col-md-4 d-flex align-items-center">
-			   <a href="index.php?page_id=1673"> 
-				<?php if( has_post_thumbnail() ):?>
-					<img src="<?php the_post_thumbnail_url('blog-small');?>" 
-						alt="<?php the_title(); ?>" 
-						class="card-img px-3 px-md-0">
-				<?php endif;?>
-				</a>
-			</div>
-			<div class="col-12 col-md-8">	
-				<div class="card-body">
-					<div class="blog-content">
-					    <p>
-							If we’ve shared a workspace, you'll know me for my long-standing appreciation of New Order, World War II knowledge and biscuit generosity. I’m a features writer, advertorial writer and sub-editor with a career that includes full-time positions at <i>GQ</i>, <i>Motor Sport</i>, <i>Grand Designs</i>, <i>Jack</i>, <i>Front</i> and <i>Amateur Photographer</i>, and freelance stints at <i>The Week Junior</i>, <i>Monocle</i>, <i>Men’s Health</i>, <i>NME</i>, <i>The Guide</i> (Guardian) and <i>Wired</i>. I’ve wangled interviews with many of my heroes and written articles on all sorts of niche subjects such as football shirts, electricity pylons and concrete lamp-posts – some of which you’ll find here. lg@leegale.co.uk.
-						</p>
+		<div class="card d-flex border-0 text-dark">
+			<div class="row no-gutters">
+				<div class="col-12 col-md-4 d-flex align-items-center">
+					<a href="index.php?page_id=1673">
+						<?php if (has_post_thumbnail()): ?>
+							<img src="<?php the_post_thumbnail_url('blog-small'); ?>"
+								alt="<?php the_title(); ?>"
+								class="card-img px-3 px-md-0">
+						<?php endif; ?>
+					</a>
+				</div>
+				<div class="col-12 col-md-8">
+					<div class="card-body">
+						<div class="blog-content">
+							<p>
+								If we’ve shared a workspace, you'll know me for my long-standing appreciation of New Order, World War II knowledge and biscuit generosity. I’m a features writer, advertorial writer and sub-editor with a career that includes full-time positions at <i>GQ</i>, <i>Motor Sport</i>, <i>Grand Designs</i>, <i>Jack</i>, <i>Front</i> and <i>Amateur Photographer</i>, and freelance stints at <i>The Week Junior</i>, <i>Monocle</i>, <i>Men’s Health</i>, <i>NME</i>, <i>The Guide</i> (Guardian) and <i>Wired</i>. I’ve wangled interviews with many of my heroes and written articles on all sorts of niche subjects such as football shirts, electricity pylons and concrete lamp-posts – some of which you’ll find here. lg@leegale.co.uk.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>		
-<?php endwhile; wp_reset_postdata(); 
-endif; ?>                                                    
- 
+<?php endwhile;
+	wp_reset_postdata();
+endif; ?>
+
 <!--/*The secondary query. */-->
 
 
-<?php 
+<?php
 $args = array(
-   'post_type' => 'post',
-   'posts_per_page' => 3,
-   'category__in'=> array(2,7,5)
-   
-   );
+	'post_type' => 'post',
+	'posts_per_page' => 3,
+	'category__in' => array(5)
+
+);
 
 $my_query = new WP_Query($args);
-if ( $my_query->have_posts() ): while ( $my_query->have_posts() ) : 
-    $my_query->the_post();
-    $do_not_duplicate = $post->ID; ?>
-	<div class="card d-flex border-0 text-dark">
-		<div class="row no-gutters">
-			<div class="col-12 col-md-4 d-flex align-items-center">
-			    <a href="<?php the_permalink(); ?>">
-				<?php if(has_post_thumbnail()):?>
-					<img src="<?php the_post_thumbnail_url('blog-small');?>" 
-						alt="<?php the_title(); ?>" 
-						class="card-img px-3 px-md-0">
-				<?php endif;?>
-			    </a>
-			</div>
-			<div class="col-12 col-md-8">	
-				<div class="card-body">
-					<div class="blog-content">
-					    <h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-						<?php the_excerpt();?>
+if ($my_query->have_posts()): while ($my_query->have_posts()) :
+		$my_query->the_post();
+		$do_not_duplicate = $post->ID; ?>
+		<div class="card d-flex border-0 text-dark">
+			<div class="row no-gutters">
+				<div class="col-12 col-md-4 d-flex align-items-center">
+					<a href="<?php the_permalink(); ?>">
+						<?php if (has_post_thumbnail()): ?>
+							<img src="<?php the_post_thumbnail_url('blog-small'); ?>"
+								alt="<?php the_title(); ?>"
+								class="card-img px-3 px-md-0">
+						<?php endif; ?>
+					</a>
+				</div>
+				<div class="col-12 col-md-8">
+					<div class="card-body">
+						<div class="blog-content">
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<?php the_excerpt(); ?>
 
-						<a class="text-secondary font-italic font-weight-bold" href="<?php the_permalink(); ?>">(Read more)</a>
+							<a class="text-secondary font-italic font-weight-bold" href="<?php the_permalink(); ?>">(Read more)</a>
+						</div>
 					</div>
 				</div>
-			</div>	
-	    </div>
-	</div>	
-<?php endwhile; wp_reset_postdata(); 
+			</div>
+		</div>
+<?php endwhile;
+	wp_reset_postdata();
 endif; ?>
-
--- LeesTheme/template-about.php
+++ LeesTheme/template-about.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Template Name: About
 */
@@ -6,50 +6,50 @@ Template Name: About
 
 <?php get_header(); ?>
 
-   <div class="container">
-        <div class='row'>
-		    <div class="col-lg-3 align-items-center">
-			<a href="index.php?page_id=55">
-                    <div class="row">
-	                    <div class="col"> 
-	                       <div class="mt-3 mt-md-4 pl-3 pb-3" id="title-box">
-	                           <h1>Lee Gale</h1>
-		                       <h3>Writer, sub-editor, advertorial copywriter</h3>
-	                       </div>
-	                   </div>
-                   </div> 
-				</a>
-                <div class="row">   
-	                <div class="w-25 mt-md-4 justify-content-left" id="nav-area">
-	                    <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
-	                        <button class="navbar-toggler mb-2" type="button" data-toggle="collapse" data-target="#navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
-	                        <span class="navbar-toggler-icon"></span>
-	                        </button>        
-	                        <?php
-	                        wp_nav_menu( array(
-	                            'theme_location'    => 'side-page-menu',
-	                            'depth'             => 2,
-	                            'container'         => 'div',
-	                            'container_class'   => 'collapse navbar-collapse',
-	                            'container_id'      => 'navbar-collapse-1',
-	                            'menu_class'        => 'nav flex-column',
-	                            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-	                            'walker'            => new WP_Bootstrap_Navwalker(),
-	                        ) );
-	                        ?> 
-	                    </nav>
-	                </div>
-                </div>
-          
-	        </div>
-            <div class="col-lg-9 px-3 p-0 mt-md-3">
-                <?php get_template_part('includes/section','about');?>
+<div class="container">
+	<div class='row'>
+		<div class="col-lg-3 align-items-center">
+			<a href="index.php?page_id=1710">
+				<div class="row">
+					<div class="col">
+						<div class="mt-3 mt-md-4 pl-3 pb-3" id="title-box">
+							<h1>Lee Gale</h1>
+							<h3>Writer, sub-editor, advertorial copywriter</h3>
+						</div>
+					</div>
+				</div>
+			</a>
+			<div class="row">
+				<div class="w-25 mt-md-4 justify-content-left" id="nav-area">
+					<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+						<button class="navbar-toggler mb-2" type="button" data-toggle="collapse" data-target="#navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<?php
+						wp_nav_menu(array(
+							'theme_location'    => 'side-page-menu',
+							'depth'             => 2,
+							'container'         => 'div',
+							'container_class'   => 'collapse navbar-collapse',
+							'container_id'      => 'navbar-collapse-1',
+							'menu_class'        => 'nav flex-column',
+							'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+							'walker'            => new WP_Bootstrap_Navwalker(),
+						));
+						?>
+					</nav>
+				</div>
+			</div>
 
-                <?php previous_posts_link();?>
-                <?php next_posts_link();?>
-            </div>
-	   </div>
-    </div>
+		</div>
+		<div class="col-lg-9 px-3 p-0 mt-md-3">
+			<?php get_template_part('includes/section', 'about'); ?>
+
+			<?php previous_posts_link(); ?>
+			<?php next_posts_link(); ?>
+		</div>
+	</div>
+</div>
 
 
 <?php get_footer(); ?>
