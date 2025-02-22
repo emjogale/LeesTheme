4f32043d41b248b7a4f2958b8f8ed2d7e19a0cb2
Emma Gale <emmajgale@hotmail.com>
2025-02-22 12:57:44 +0000

    update includes/front-page section to show 2 featured posts followed by latest post

--- LeesTheme/includes/section-frontpage.php
+++ LeesTheme/includes/section-frontpage.php
@@ -22,10 +22,29 @@
 				</div>
 			</div>
 		</div>
+<?php endwhile;
+	wp_reset_postdata();
+endif; ?>
+<!-- end first query here  -->
+
+<!--/*The secondary query. */-->
+
+<?php
+$args = array(
+	'post_type' => 'post',
+	'posts_per_page' => 2,
+	'category__in' => array(5)
+
+);
+
+$my_query = new WP_Query($args);
+if ($my_query->have_posts()): while ($my_query->have_posts()) :
+		$my_query->the_post();
+		$do_not_duplicate = $post->ID; ?>
 		<div class="card d-flex border-0 text-dark">
 			<div class="row no-gutters">
 				<div class="col-12 col-md-4 d-flex align-items-center">
-					<a href="index.php?page_id=1673">
+					<a href="<?php the_permalink(); ?>">
 						<?php if (has_post_thumbnail()): ?>
 							<img src="<?php the_post_thumbnail_url('blog-small'); ?>"
 								alt="<?php the_title(); ?>"
@@ -36,29 +55,10 @@
 				<div class="col-12 col-md-8">
 					<div class="card-body">
 						<div class="blog-content">
-							<p>One of the unsung architects of the Manchester sound, the Salford stalwart has influenced every major musical movement of the past 35 years. Whether pioneering post-punk with Joy Division, melding rock/dance with New Order, or blowing £1m on a nightclub, “Barney” was there. As his band limber up for an Olympic concert, GQ pays tribute to the straight man of Madchester</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="card d-flex border-0 text-dark">
-			<div class="row no-gutters">
-				<div class="col-12 col-md-4 d-flex align-items-center">
-					<a href="index.php?page_id=1673">
-						<?php if (has_post_thumbnail()): ?>
-							<img src="<?php the_post_thumbnail_url('blog-small'); ?>"
-								alt="<?php the_title(); ?>"
-								class="card-img px-3 px-md-0">
-						<?php endif; ?>
-					</a>
-				</div>
-				<div class="col-12 col-md-8">
-					<div class="card-body">
-						<div class="blog-content">
-							<p>
-								If we’ve shared a workspace, you will know me for my appreciation of New Order, World War II knowledge and biscuit generosity. I’m a writer/sub-editor with a 30-year career that includes full-time positions at <em>GQ</em>, <em>Motor Sport</em>, <em>Jack and</em> <em>Front</em>, and freelance stints at <em>Monocle</em>, <em>NME and</em> <em>The Guide</em> (<em>Guardian</em>). I’ve wangled interviews with many of my heroes and written articles on many niche subjects such as electricity pylons and concrete lamp-posts. I’ve just finished a novel – a ghost yarn titled <em>Brian Clough, Geoff Boycott, Peter O’Toole, Tony Wilson, Mark E Smith And Tim Healy In A Haunted House Near Carlisle, 1984</em>.
-							</p>
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<?php the_excerpt(); ?>
+
+							<a class="text-secondary font-italic font-weight-bold" href="<?php the_permalink(); ?>">(Read more)</a>
 						</div>
 					</div>
 				</div>
@@ -68,14 +68,13 @@
 	wp_reset_postdata();
 endif; ?>
 
-<!--/*The secondary query. */-->
-
+<!--/*The third query. */-->
 
 <?php
 $args = array(
 	'post_type' => 'post',
 	'posts_per_page' => 1,
-	'category__in' => array(5)
+	'orderby' => 'post_date'
 
 );
 
